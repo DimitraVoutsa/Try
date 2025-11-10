1c4ebbb30c3d839c72e2b4bfa7ec319dafb2f176
DimitraVoutsa <t8240019@aueb.gr>
2025-11-10 20:23:25 +0200

    Diorthwsh

--- Esoda.java
+++ Esoda.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-public class Esoda {
+public class Esoda {     
     public static int getExit() {
         Scanner scanner = new Scanner(System.in);
-        return "Insert the number";
+        System.out.println("Insert the number");
         int num = new scanner.nextInt();
-        return "The number you have inserted is: " + num;
+        System.out.println("The number you have inserted is: ");
+        return num;
     }
 }
